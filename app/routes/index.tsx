6bbcc54c9a9a7242e870a24c6eddc084d94b20e6
tigerabrodi <tigerabrodi@gmail.com>
2022-01-12 19:57:26 +0100

    feat: add initial logic to main page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,31 @@
+import * as React from "react";
+import { colors } from "~/theme";
+import { wsContext } from "~/ws-context";
+
 export default function Index() {
+  const [text, setText] = React.useState("");
+  const socket = React.useContext(wsContext);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+    socket!.emit("something", "ping");
+  };
+
+  React.useEffect(() => {
+    if (!socket) return;
+
+    socket.on("event", (data) => {
+      console.log(data);
+    });
+
+    socket.emit("something", "ping");
+  }, [socket]);
+
   return (
     <main
       style={{
-        height: "100vh",
-        width: "100vw",
+        height: "100%",
+        width: "100%",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
@@ -13,14 +35,44 @@ export default function Index() {
       <h1
         style={{
           fontSize: 40,
-          color: "black",
+          color: colors.red,
           fontWeight: "bold",
           fontFamily: "sans-serif",
-          marginTop: 40,
+          marginTop: 25,
         }}
       >
         Extremely Simple Google Docs
       </h1>
+      <label
+        htmlFor="text"
+        style={{
+          fontSize: 25,
+          marginTop: 20,
+          color: colors.pink,
+          textAlign: "center",
+          fontFamily: "sans-serif",
+          fontWeight: 500,
+        }}
+      >
+        Text
+      </label>
+      <textarea
+        name="text"
+        id="text"
+        value={text}
+        onChange={handleChange}
+        style={{
+          width: "80vw",
+          maxWidth: 800,
+          height: 900,
+          marginTop: 30,
+          color: colors.orange,
+          backgroundColor: colors.brown,
+          fontSize: 20,
+          padding: 20,
+          border: `2px solid ${colors.pink}`,
+        }}
+      ></textarea>
     </main>
   );
 }
