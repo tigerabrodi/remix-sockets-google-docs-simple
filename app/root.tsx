cb9f0c9bcb658229e720b9d4630a85c5e82a4e68
tigerabrodi <tigerabrodi@gmail.com>
2022-01-12 09:38:42 +0100

    setup

---
+++ app/root.tsx
@@ -0,0 +1,57 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import React from "react";
+import type { MetaFunction } from "remix";
+import { connect } from "./ws-client";
+import { Socket } from "socket.io-client";
+import { wsContext } from "./ws-context";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+
+export const meta: MetaFunction = () => {
+  return { title: "Simple Google Docs" };
+};
+
+export default function App() {
+  let [socket, setSocket] =
+    React.useState<Socket<DefaultEventsMap, DefaultEventsMap>>();
+
+  React.useEffect(() => {
+    let connection = connect();
+    setSocket(connection);
+    return () => {
+      connection.close();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (!socket) return;
+    socket.on("event", (data) => {
+      console.log(data);
+    });
+  }, [socket]);
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <wsContext.Provider value={socket}>
+          <Outlet />
+        </wsContext.Provider>
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,26 @@
+export default function Index() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 40,
+          color: "black",
+          fontWeight: "bold",
+          fontFamily: "sans-serif",
+          marginTop: 40,
+        }}
+      >
+        Extremely Simple Google Docs
+      </h1>
+    </main>
+  );
+}
