c2422c63ed1068aec7550b82b7750cc6fe8e9aeb
tigerabrodi <tigerabrodi@gmail.com>
2022-01-15 08:33:11 +0100

    test: add test when multiple clients join a room

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 const randomText = 'some random text'
 
-test('Type in two browsers, landing page.', async ({ browser }) => {
+test('Two clients typing on the landing page.', async ({ browser }) => {
   const context = await browser.newContext()
 
   const pageOne = await context.newPage()
@@ -10,6 +10,26 @@ test('Type in two browsers, landing page.', async ({ browser }) => {
 
   await pageOne.goto('http://localhost:3000/')
   await pageTwo.goto('http://localhost:3000/')
+
+  // Type in page one
+  await pageOne.fill('textarea', randomText)
+
+  // Assert text page two
+  const $twoTextarea = pageTwo.locator('textarea')
+  await expect($twoTextarea).toHaveText(randomText)
+
+  await context.close()
+})
+
+test('Two clients typing in a room', async ({ browser }) => {
+  const context = await browser.newContext()
+
+  const pageOne = await context.newPage()
+  const pageTwo = await context.newPage()
+
+  const room = 1
+  await pageOne.goto(`http://localhost:3000/${room}`)
+  await pageTwo.goto(`http://localhost:3000/${room}`)
 
   // Type in page one
   await pageOne.fill('textarea', randomText)
